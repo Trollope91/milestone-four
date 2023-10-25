5e20cb7124f3ba81b180d1e3367fea6b9c8d347e
Trollope91 <ljtdev91@gmail.com>
2023-10-25 12:33:11 +0000

    Create html for shop page

--- trainwithluke/core/views.py
+++ trainwithluke/core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from product.models import Product
+
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    products = Product.objects.all() [0:8]
+    return render(request, 'core/frontpage.html', {'products': products})
+
+def shop(request):
+    products = Product.objects.all()
+    return render(request, 'core/shop.html', {'products': products})
