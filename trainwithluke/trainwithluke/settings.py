7a60dacf35806d284d9641d85c67d3602a83bcc7
Trollope91 <ljtdev91@gmail.com>
2023-10-30 10:32:34 +0000

    Create django app for the cart

--- trainwithluke/trainwithluke/settings.py
+++ trainwithluke/trainwithluke/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart',
     'core',
     'product',
-
 ]
 
 MIDDLEWARE = [
