ab178671f5e3c572c2c66576e1a97e257fc5cf2f
Trollope91 <ljtdev91@gmail.com>
2023-10-30 13:06:25 +0000

    Create login page html

--- trainwithluke/trainwithluke/urls.py
+++ trainwithluke/trainwithluke/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.urls import path
 
 from cart.views import add_to_cart
-from core.views import frontpage, shop, signup
+from core.views import frontpage, shop, signup, login
 from product.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
     path('add_to_cart/<int:product_id>', add_to_cart, name='add_to_cart'),
